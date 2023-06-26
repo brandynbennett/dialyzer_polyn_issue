3024eeedcbc1ab0e396f05f9a89186d919ccadb2
brandynbennett <brandynlbennett@gmail.com>
2023-06-26 17:30:17 -0600

    init

---
+++ lib/dialyzer_polyn_issue.ex
@@ -0,0 +1,18 @@
+defmodule DialyzerPolynIssue do
+  @moduledoc """
+  Documentation for `DialyzerPolynIssue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DialyzerPolynIssue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dialyzer_polyn_issue_test.exs
@@ -0,0 +1,8 @@
+defmodule DialyzerPolynIssueTest do
+  use ExUnit.Case
+  doctest DialyzerPolynIssue
+
+  test "greets the world" do
+    assert DialyzerPolynIssue.hello() == :world
+  end
+end
